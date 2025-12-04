7d87388172f4fe83a3362523e11ae00a06129a1a
eceCaliskan <ece98caliskan@gmail.com>
2025-12-04 20:55:09 +0000

    feat: add active_key_duration_check function to scanner

--- backend/file_reader.py
+++ backend/file_reader.py
@@ -1,5 +1,5 @@
 import json
-from ec2_scanner import ec2_ip_check, ec2_open_ports_check, mfa_check
+from ec2_scanner import ec2_ip_check, ec2_open_ports_check, mfa_check, active_key_duration_check
 from collections import defaultdict
 
 # Path to mock AWS data
@@ -14,16 +14,12 @@ def start_ec2_scanner():
     with open(FILE_PATH, encoding="utf-8") as file:
         aws_data = json.load(file)
 
-    
     account_ids = set(item.get("AccountId", "UnknownAccount") for item in aws_data)
-
-  
     grouped_data = defaultdict(list)
+
     for item in aws_data:
         account_id = item.get("AccountId", "UnknownAccount")
         grouped_data[account_id].append(item)
-
-   
     for account_id in account_ids:
         print('-----------------------------------------')
         print(f"\n 🔍 Scanning EC2 instances for Account: {account_id}: \n")
@@ -34,6 +30,7 @@ def start_ec2_scanner():
                 ec2_open_ports_check(resource)
             if resource.get("Type") == "IAM":
                 mfa_check(resource)
+                active_key_duration_check(resource)
             
 
 if __name__ == "__main__":
