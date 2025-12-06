9a338c3ff12f1536d462ffd7b11131872625f7a5
eceCaliskan <ece98caliskan@gmail.com>
2025-12-06 22:13:26 +0000

    feat: implement logic to check if S3 instance is publicly accessible

---
+++ backend/s3_scanner.py
@@ -0,0 +1,10 @@
+def s3_access_check(s3_instance):
+    access_grants = s3_instance.get('Acl').get('Grants')
+    for access_grant in access_grants:
+        if access_grant.get('Grantee').get('URI') == "http://acs.amazonaws.com/groups/global/AllUsers":
+             print(f"🚨 Bucket {s3_instance.get('BucketName')} is exposed to AllUsers with "
+                        f"{access_grant.get('Permission')} permission"
+              )
+
+
+  
